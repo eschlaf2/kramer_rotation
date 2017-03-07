0db12c5929f03a72dbe0f79f45441650d2f35fdd
Emily <eschlaf2@illinois.edu>
2017-03-07 15:33:09 -0500

    fixed qsub file

--- epileptor_bluepy/optimize_epileptor_qsub.py
+++ epileptor_bluepy/optimize_epileptor_qsub.py
@@ -1,17 +1,29 @@
 import bluepyopt as bpop
 import epileptor_evaluator
-from scipy import linalg as la
-import matplotlib.pyplot as plt
+import pickle
+
+
+def save_obj(obj, name):
+    with open('obj/' + name + '.pkl', 'wb') as f:
+        pickle.dump(obj, f, pickle.HIGHEST_PROTOCOL)
+
+
+def load_obj(name):
+    with open('obj/' + name + '.pkl', 'rb') as f:
+        return pickle.load(f)
+
 
 if __name__ == '__main__':
-	hall_of_fame = []
-	for i in range(1):
+    hall_of_fame = []
+    for i in range(1):
         evaluator = epileptor_evaluator.Epileptor_Evaluator(plot=False,
                                                             total_time=250,
                                                             dt_sample=0.1)
-        opt = bpop.optimisations.DEAPOptimisation(evaluator, offspring_size=3,
-                                                  eta=20, mutpb=0.3, cxpb=0.7,
-                                                  seed=i, use_scoop=True)
-        final_pop, hof_temp, log, hst = opt.run(max_ngen=3)
-        hall_of_fame.append(hof_temp[0])
-np.save('test_run', hall_of_fame)
+    opt = bpop.optimisations.DEAPOptimisation(evaluator, offspring_size=3,
+                                              eta=20, mutpb=0.3, cxpb=0.7,
+                                              seed=i, use_scoop=True)
+    final_pop, hof_temp, log, hst = opt.run(max_ngen=3)
+    hall_of_fame.append(hof_temp[0])
+    params = evaluator.get_param_dict(hall_of_fame[0])
+    save_obj(params, 'params')
+    save_obj(hall_of_fame, 'hall_of_fame')
